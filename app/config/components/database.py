1b3a99664ab309e857ae350757ec98d94e8035ff
tier <tier1985@gmail.com>
2023-09-07 16:21:26 +0200

    SQLITE added

--- app/config/components/database.py
+++ app/config/components/database.py
@@ -1,18 +1,27 @@
 import os
 
+from app.config.settings import BASE_DIR
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        # 'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
-        # 'HOST': 'db',
-        # 'HOST': 'localhost',
-        'HOST': 'pykids_db',
-        'PORT': os.environ.get('DB_PORT', 5432),
-        'OPTIONS': {
-            'options': '-c search_path=public,content'
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD'),
+#         # 'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
+#         # 'HOST': 'db',
+#         # 'HOST': 'localhost',
+#         'HOST': 'pykids_db',
+#         'PORT': os.environ.get('DB_PORT', 5432),
+#         'OPTIONS': {
+#             'options': '-c search_path=public,content'
+#         }
+#     }
+# }
