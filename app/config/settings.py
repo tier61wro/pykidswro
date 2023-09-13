68ae820fa9b5b94a1888497de6622008a7e658be
tier <tier1985@gmail.com>
2023-09-13 19:06:20 +0200

    fix static

--- app/config/settings.py
+++ app/config/settings.py
@@ -117,7 +117,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATIC_ROOT = '/app/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static/')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "../static")]
 
 
 # Default primary key field type
