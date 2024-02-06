9db4cc9e466cd083218ddbec7c48d6a2a51b3aac
tier <tier1985@gmail.com>
2024-02-06 16:04:17 +0100

    remove useless files and dirs

--- app/main/urls.py
+++ app/main/urls.py
@@ -6,7 +6,6 @@ app_name = 'main'
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
     path('index/', IndexView.as_view(), name='index'),
-    path('about/', AboutView.as_view(), name='about'),
     path('course/', CourseView.as_view(), name='course'),
     path('contact/', ContactView.as_view(), name='contact'),
     # path('profile/', ProfileView.as_view(), name='profile'),
