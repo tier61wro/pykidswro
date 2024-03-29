10503a3553a07d2147a30dd5fccf148cc06780d2
tier <tier1985@gmail.com>
2024-03-29 12:55:51 +0100

    update

--- app/lessons/admin.py
+++ app/lessons/admin.py
@@ -1,14 +1,23 @@
 # Register your models here.
 from django.contrib import admin
+from django.template.defaultfilters import truncatechars  # or truncatewords
 
 from .models import Chapter, Lesson, UserLessonProgress
 
 
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ('order', 'chapter_order', 'chapter', 'title', 'content')  # Пример отображения полей модели
+    list_display = ('order', 'chapter_order', 'chapter', 'title', 'short_content')
+
+    # хак чтобы поле content не было слишком большим в админке
+    def short_content(self, obj):
+        return truncatechars(obj.content, 100)  # Обрезает содержимое до 100 символов
+
+    # Заголовок столбца в админке
+    short_content.short_description = 'Content'  # type: ignore[attr-defined]
 
 
 admin.site.register(Lesson, LessonAdmin)
+
 
 
 class UserLessonProgressAdmin(admin.ModelAdmin):
---
+++ scripts/populate_lessons.py
@@ -0,0 +1,110 @@
+import os
+import re
+import sys
+
+import django
+import requests
+from django.core.exceptions import ObjectDoesNotExist
+
+# Добавляем в sys.PATH путь до каталога, где находится manage.py
+# Предполагая, что скрипт запускается из каталога, который на один уровень выше, чем 'app'
+sys.path.append(os.path.join(os.path.dirname(__file__), '../app'))
+
+# Указываем путь к вашему файлу settings.py
+os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings'
+
+# Настраиваем Django
+django.setup()
+
+# Теперь можно импортировать и использовать модели
+from lessons.models import Chapter, Lesson
+
+# Устанавливаем базовый URL для API запроса
+REPO_NAME = "tier61wro/pykids_theory"
+BRANCH = "master"
+PATH = "pl"  # Путь в репозитории
+api_url = f"https://api.github.com/repos/{REPO_NAME}/contents/{PATH}?ref={BRANCH}"
+
+
+def replace_markdown_tags(text):
+    REPLACEMENTS = {
+        re.compile(r':warning:'): '⚠️',
+        re.compile(r':question:'): '❓',
+    }
+
+    for regex, replacement in REPLACEMENTS.items():
+        text = regex.sub(replacement, text)
+    return text
+
+
+def extract_number_from_filename(filename):
+    # Используем регулярное выражение для поиска числа в начале имени файла
+    match = re.search(r'^_?(\d+)_', filename)
+    # Если найдено, преобразуем в целое число и возвращаем
+    if match:
+        return int(match.group(1))
+    # Если число в имени файла отсутствует, возвращаем None
+    return None
+
+
+def get_file_content(filename):
+    raw_url = "https://raw.githubusercontent.com/tier61wro/pykids_theory/master/pl/"
+    url = f"{raw_url}{filename}"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.text
+    else:
+        raise Exception(f"Ошибка при доступе к {url}: {response.status_code}")
+
+
+def get_lesson_title(content):
+    try:
+        lines = content.split("\n")
+        first_line = lines[0]
+        if ":" in first_line:  # Проверяем, содержит ли первая строка двоеточие
+            title = first_line.split(":", 1)[1].strip()
+            lesson_content = "\n".join(lines[1:])
+            return title, lesson_content
+        else:
+            raise ValueError("Title format not recognized")  # Явно выбрасываем исключение, если формат не соответствует
+    except (IndexError, ValueError) as e:
+        print(f"Error processing lesson content: {e}")  # Обрабатываем известные исключения
+        return None, None
+
+
+if __name__ == "__main__":
+    # Делаем запрос к GitHub API
+    response = requests.get(api_url)
+    Lesson.objects.all().delete()
+
+    # Проверяем статус ответа
+    if response.status_code == 200:
+        # Забираем данные о файлах
+        files_data = response.json()
+
+        # Печатаем имена файлов
+
+        for file in files_data:
+            if file['type'] == 'file' and file['name'].endswith('.md'):
+                lesson_file_name = file['name']
+                lesson_order = extract_number_from_filename(lesson_file_name)
+                file_content = get_file_content(lesson_file_name)
+                chapter = Chapter.objects.get(number=0)
+                chapter_order = lesson_order
+                lesson_title, lesson_content = get_lesson_title(file_content)
+                lesson_content = replace_markdown_tags(lesson_content)
+                if not lesson_title:
+                    print(f"ERROR: can't find title for lesson {lesson_file_name}, check content")
+
+                lesson = Lesson.objects.create(
+                    order=lesson_order,
+                    chapter_order=chapter_order,
+                    chapter=chapter,
+                    title=lesson_title,
+                    content=lesson_content
+                )
+
+    else:
+        print("Не удалось получить данные от GitHub API")
